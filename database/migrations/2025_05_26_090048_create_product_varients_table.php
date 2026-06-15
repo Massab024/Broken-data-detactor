3e3c50cdafdc55ca77fb61073b37a3b5f291ed4e
Syed Umer <umer@swishtag.com>
2026-06-15 13:34:26 +0500

    worker change

--- database/migrations/2025_05_26_090048_create_product_varients_table.php
+++ database/migrations/2025_05_26_090048_create_product_varients_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('product_varients', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id')->nullable();
+            // $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->unsignedBigInteger('shopify_product_varient_id')->nullable();
             $table->unsignedBigInteger('shopify_inventory_item_id')->nullable();
             $table->double('compare_at_price')->nullable();
